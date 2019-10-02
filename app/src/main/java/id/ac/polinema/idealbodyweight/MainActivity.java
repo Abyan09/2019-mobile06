d847bfff24c89165df0c8c869c95fc897cd6146a
Abyan <harlem.minion09@gmail.com>
2019-10-02 14:40:31 +0700

    Bundle

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,14 +7,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import id.ac.polinema.fragments.AboutFragment;
+
 public class MainActivity extends AppCompatActivity {
 
+	private AboutFragment aboutFragment;
 	// Deklarasikan atribut Fragment di sini
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		aboutFragment = AboutFragment.newInstance("Moch. Abyan An-nabhany");
 	}
 
 	@Override
@@ -26,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
-
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, aboutFragment)
+				.addToBackStack(null)
+				.commit();
 		return super.onOptionsItemSelected(item);
 	}
 }
