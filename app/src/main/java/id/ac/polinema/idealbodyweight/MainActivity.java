ab1cfea54f0fad32e60c14ce9114f7916a6d7262
Abyan <harlem.minion09@gmail.com>
2019-10-09 12:05:38 +0700

    BMI & Result Fragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -8,18 +8,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import id.ac.polinema.fragments.AboutFragment;
+import id.ac.polinema.fragments.BMIFragment;
 import id.ac.polinema.fragments.BrocaIndexFragment;
 import id.ac.polinema.fragments.MenuFragment;
 import id.ac.polinema.fragments.ResultFragment;
 
 public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener,
         BrocaIndexFragment.OnFragmentInteractionListener,
-		ResultFragment.OnFragmentInteractionListener{
+		ResultFragment.OnFragmentInteractionListener,
+		BMIFragment.OnFragmentInteractionListener{
 
 	private MenuFragment menuFragment;
 	private AboutFragment aboutFragment;
     private BrocaIndexFragment brocaIndexFragment;
 	private ResultFragment resultFragment;
+	private BMIFragment bmiFragment;
 	// Deklarasikan atribut Fragment di sini
 
 	@Override
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, menuFragment)
 				.commit();
+		bmiFragment = new BMIFragment();
 	}
 
 	@Override
@@ -55,12 +59,16 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 	public void onBrocaIndexButtonClicked() {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, brocaIndexFragment)
-                .commit();
+				.addToBackStack(null)
+				.commit();
 	}
 
 	@Override
 	public void onBodyMassIndexButtonClicked() {
-
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, bmiFragment)
+				.addToBackStack(null)
+				.commit();
 	}
 
     @Override
@@ -68,13 +76,29 @@ public class MainActivity extends AppCompatActivity implements MenuFragment.OnFr
 		resultFragment.setInformation(String.format("Your ideal weight is %.2f kg", index));
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, resultFragment)
+				.replace(R.id.fragment_container, resultFragment, "BROCA")
 				.commit();
     }
 
 	@Override
 	public void onTryAgainButtonClicked(String tag) {
+		if (tag.equals("BROCA")) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, brocaIndexFragment)
+					.commit();
+		}else if(tag.equals("BMI")){
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, bmiFragment)
+					.commit();
+		}
+	}
+
+	@Override
+	public void onCalculateBMIClicked(String result) {
+		resultFragment.setInformation(String.format("Your healthy BMI range is "+result));
 		getSupportFragmentManager().beginTransaction()
 				.replace(R.id.fragment_container, brocaIndexFragment)
+				.replace(R.id.fragment_container, resultFragment, "BMI")
 				.commit();
 	}
 }
